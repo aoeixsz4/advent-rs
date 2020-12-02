ffe9ba2056e636d97bfec41340c00f8c5adc4371
Joanna Doyle <jjadoyle@gmail.com>
2020-12-02 09:37:25 +0100

    add src/input.rs for reading inputs

---
+++ src/input.rs
@@ -0,0 +1,15 @@
+use std::io::{self, BufRead, BufReader};
+use std::fs::File;
+
+pub fn get_numeric_input(day: &str) -> Result<Vec<u32>, io::Error> {
+    let mut output = Vec::new();
+    let file_handle = File::open(format!("inputs/{}", day))?;
+    let reader = BufReader::new(file_handle);
+    for try_line in reader.lines() {
+        match try_line {
+            Ok(line) => output.push(line.parse::<u32>().expect("invalid input format")),
+            Err(e) => return Err(e)
+        }
+    }
+    Ok(output)
+}
