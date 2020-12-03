9a00a1d00868f770a801879fdb731d75f9d7c613
Joanna Doyle <jjadoyle@gmail.com>
2020-12-03 10:48:50 +0100

    day3 part2 star GET

--- src/day3.rs
+++ src/day3.rs
@@ -6,13 +6,32 @@ pub fn solve() -> Result<(), io::Error> {
         .expect("couldn't open input file inputs/day3");
     let mut tree_count = 0;
     let mut absolute_horizontal_index = 0;
-    for line in input_lines {
-        let index = absolute_horizontal_index % line.len();
-        if line.chars().nth(index).unwrap() == '#' {
+    for j in 0 .. input_lines.len() {
+        let index = absolute_horizontal_index % input_lines[j].len();
+        if input_lines[j].chars().nth(index).unwrap() == '#' {
             tree_count += 1;
         }
         absolute_horizontal_index += 3;
     }
     println!("part1: {}", tree_count);
+
+    // now for part 2
+    let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let mut answer: u64 = 1;
+    for k in 0 .. slopes.len() {
+        let mut j = 0;
+        let mut i = 0;
+        let mut count = 0;
+        while j < input_lines.len() {
+            let index = i % input_lines[j].len();
+            if input_lines[j].chars().nth(index).unwrap() == '#' {
+                count += 1;
+            }
+            i += slopes[k].0;
+            j += slopes[k].1;
+        }
+        answer *= count;
+    }
+    println!("part2: {}", answer);
     Ok(())
 }
