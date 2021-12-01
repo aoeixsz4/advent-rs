787252da95cd95e5413ac06ae331e0d48301065f
Dr. Joanna Irina Zaitseva-Kinneberg <jjadoyle@gmail.com>
2021-12-01 23:36:17 +0100

    AoC 2021 let's goooooooooo

--- src-2020/main.rs
+++ src-2020/main.rs
@@ -1,6 +1,6 @@
 mod day1;
-//mod day2;
-//mod day3;
+mod day2;
+mod day3;
 mod input;
 
 use chrono::{Datelike, Utc};
@@ -14,8 +14,8 @@ fn main() -> Result<(), io::Error> {
     for day in days {
         match day.as_str() {
             "1" | "day1" => { println!("\t\tDAY ONE"); day1::solve()? },
-            //"2" | "day2" => { println!("\t\tDAY TWO"); day2::solve()? },
-            //"3" | "day3" => { println!("\t\tDAY TWO"); day3::solve()? },
+            "2" | "day2" => { println!("\t\tDAY TWO"); day2::solve()? },
+            "3" | "day3" => { println!("\t\tDAY TWO"); day3::solve()? },
             _ => writeln!(io::stderr(), "unknown argument: {}", &day)?
         }
         println!("");
--- src/day1.rs
+++ src/day1.rs
@@ -1,42 +1,34 @@
 use std::io::{self, Write};
 use crate::input;
 
-fn part1(data: &[u32], target: u32) -> Option<u32> {
-    let (mut i, mut k) = (0, data.len() - 1);
-    while i < k {
-        let test = data[i] + data[k];
-        if test > target {
-            k -= 1;
-        } else if test < target {
-            i += 1;
-        } else {
-            return Some(data[i] * data[k]);
+fn part1(data: &[u32]) -> u32 {
+    let (mut i, n, mut count) = (1, data.len(), 0);
+    while i < n {
+        if data[i] > data[i-1] {
+            count += 1;
         }
+        i += 1;
     }
-    None
+    count
 }
 
-fn part2(data: &[u32]) -> Option<u32> {
-    for i in 0 .. data.len() {
-        match part1(data, 2020 - data[i]) {
-            Some(part_solution) => return Some(data[i] * part_solution),
-            None => ()
-        }
-    }
-    None
-}
+//fn part2(data: &[u32]) -> Option<u32> {
+//    for i in 0 .. data.len() {
+//        match part1(data, 2020 - data[i]) {
+//            Some(part_solution) => return Some(data[i] * part_solution),
+//            None => ()
+//        }
+//    }
+//    None
+//}
 
 pub fn solve() -> Result<(), io::Error> {
     let mut data = input::get_numeric_input("day1")
         .expect("couldn't open input file for day1 (should be inputs/day1)");
-    data.sort(); // both part 1 and part 2 rely on a sorted array
-    match part1(data.as_slice(), 2020) {
-        Some(solution) => println!("part1: {}", solution.to_string()),
-        None => writeln!(io::stderr(), "attempt to solve day 1, part 1 failed")?
-    }
-    match part2(data.as_slice()) {
-        Some(solution) => println!("part2: {}", solution.to_string()),
-        None => writeln!(io::stderr(), "attempt to solve day 1, part 2 failed")?
-    }
+    println!("part1: {}", part1(data.as_slice()));
+    //match part2(data.as_slice()) {
+    //    Some(solution) => println!("part2: {}", solution.to_string()),
+    //    None => writeln!(io::stderr(), "attempt to solve day 1, part 2 failed")?
+    //}
     Ok(())
 }
