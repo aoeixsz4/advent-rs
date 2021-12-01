f1757acb94a96233dde057630ef297299227664d
Dr. Joanna Irina Zaitseva-Kinneberg <jjadoyle@gmail.com>
2021-12-02 00:01:40 +0100

    second star get :3

--- src/day1.rs
+++ src/day1.rs
@@ -7,23 +7,15 @@ fn part1(data: &[u32]) -> u32 {
     })
 }
 
-//fn part2(data: &[u32]) -> Option<u32> {
-//    for i in 0 .. data.len() {
-//        match part1(data, 2020 - data[i]) {
-//            Some(part_solution) => return Some(data[i] * part_solution),
-//            None => ()
-//        }
-//    }
-//    None
-//}
+fn part2(data: &[u32]) -> u32 {
+    let sliding_window_sums: Vec<u32> = data.windows(3).map(|y| y.iter().sum()).collect();
+    part1(sliding_window_sums.as_slice())
+}
 
 pub fn solve() -> Result<(), io::Error> {
     let data = input::get_numeric_input("day1")
         .expect("couldn't open input file for day1 (should be inputs/day1)");
     println!("part1: {}", part1(&data));
-    //match part2(data.as_slice()) {
-    //    Some(solution) => println!("part2: {}", solution.to_string()),
-    //    None => writeln!(io::stderr(), "attempt to solve day 1, part 2 failed")?
-    //}
+    println!("part2: {}", part2(&data));
     Ok(())
 }
