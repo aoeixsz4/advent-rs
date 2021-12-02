4a959fae560aea1b0fd999661f4787d6abea5eeb
Dr. Joanna Irina Zaitseva-Kinneberg <jjadoyle@gmail.com>
2021-12-02 08:07:48 +0100

    Dec 2 pt2 star get :>

--- src/day2.rs
+++ src/day2.rs
@@ -6,11 +6,13 @@ struct Position {
     depth: i64
 }
 
+struct SubStatus {
+    pos: Position,
+    aim: i64
+}
+
 fn part1(data: &[String]) -> i64 {
-    let mut pos = Position {
-        horizontal: 0,
-        depth: 0
-    };
+    let mut pos = Position { horizontal: 0, depth: 0 };
     data.iter().map(|l| l.split_once(' '))
         .for_each(|s| {
             if let Some((direction, amount)) = s {
@@ -25,9 +27,32 @@ fn part1(data: &[String]) -> i64 {
     pos.horizontal * pos.depth
 }
 
+fn part2(data: &[String]) -> i64 {
+    let mut status = SubStatus {
+        pos: Position { horizontal: 0, depth: 0 },
+        aim: 0
+    };
+    data.iter().map(|l| l.split_once(' '))
+        .for_each(|s| {
+            if let Some((direction, amount)) = s {
+                match direction {
+                    "forward" => {
+                        status.pos.horizontal += amount.parse::<i64>().unwrap();
+                        status.pos.depth += status.aim * amount.parse::<i64>().unwrap();
+                    },
+                    "up" => status.aim -= amount.parse::<i64>().unwrap(),
+                    "down" => status.aim += amount.parse::<i64>().unwrap(),
+                    _ => ()
+                }
+            }
+        });
+    status.pos.horizontal * status.pos.depth
+}
+
 pub fn solve() -> Result<(), io::Error> {
     let data = input::get_lines_input("day2")
         .expect("couldn't open input file for day2 (should be inputs/day2)");
     println!("part1: {}", part1(&data));
+    println!("part2: {}", part2(&data));
     Ok(())
 }
