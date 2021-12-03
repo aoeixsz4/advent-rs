e2b136071d97fb551387726f8d27778bd1c2cae5
Joanna Doyle <jdoyle@epages.com>
2021-12-03 16:19:37 +0100

    refactor common elements

--- src/day3.rs
+++ src/day3.rs
@@ -5,18 +5,22 @@ use bitvec::prelude::*;
 const NR_BITS: usize = 12;
 const BITMASK: u16 = 0xfff;
 
-fn part1(data: &[String]) -> i64 {
+fn common(data: &[String]) -> [usize; NR_BITS] {
     let mut bit_counts: [usize; NR_BITS] = [0; NR_BITS];
-    let len = data.len();
     data.iter().for_each(|bitstring| {
         bitstring.char_indices().take(NR_BITS).filter(|(_, c)| *c == '1')
         .for_each(|(i, _)| {
             bit_counts[i] += 1
         })
     });
-    let bitvec = bit_counts.map(|n| n > len/2)
-        .iter().collect::<BitVec<Msb0, u16>>();
-    let gamma = bitvec.as_raw_slice()[0] >> (16 - NR_BITS);
+    bit_counts
+}
+
+fn part1(data: &[String]) -> i64 {
+    let bit_counts = common(data);
+    let bit_vector: BitVec<Msb0, u16> = bit_counts.map(|n| n > data.len()/2)
+        .iter().collect();
+    let gamma = bit_vector.as_raw_slice()[0] >> (16 - NR_BITS);
     println!("gamma: {:#x}", gamma);
     let epsilon = gamma ^ BITMASK;
     gamma as i64 * epsilon as i64
