05c9d80ea4cc95185acd74208f2e65a795632de0
Joanna Doyle <jdoyle@epages.com>
2021-12-13 07:32:59 +0100

    day13 pt2

--- src/day13.rs
+++ src/day13.rs
@@ -1,7 +1,9 @@
 use std::collections::HashSet;
 const INPUT: &str = include_str!("day13.txt");
+const HEIGHT: usize = 40;
+const WIDTH: usize = 200;
 
-fn fold_paper(input: &str, fold_once: bool) -> usize {
+fn fold_paper(input: &str, fold_once: bool) -> HashSet<(usize, usize)> {
     let (dots, mirrors) = input.split_once("\n\n").unwrap();
     let mut dots_set = HashSet::new();
     dots.lines().map(|l| l.split_once(",").unwrap())
@@ -14,7 +16,7 @@ fn fold_paper(input: &str, fold_once: bool) -> usize {
         match axis {
             "x" => {
                 let dots_copy =  dots_set.clone();
-                for (x, y) in dots_copy.into_iter().filter(|(x, _y)| *x > m + 1) {
+                for (x, y) in dots_copy.into_iter().filter(|(x, _y)| *x > m) {
                     dots_set.remove(&(x, y));
                     if x-m <= m {
                         dots_set.insert((m - (x-m), y));
@@ -23,7 +25,7 @@ fn fold_paper(input: &str, fold_once: bool) -> usize {
             },
             "y" => {
                 let dots_copy =  dots_set.clone();
-                for (x, y) in dots_copy.into_iter().filter(|(_x, y)| *y > m + 1) {
+                for (x, y) in dots_copy.into_iter().filter(|(_, y)| *y > m) {
                     dots_set.remove(&(x, y));
                     if y-m <= m {
                         dots_set.insert((x, m - (y - m)));
@@ -34,15 +36,32 @@ fn fold_paper(input: &str, fold_once: bool) -> usize {
         }
         if fold_once { break; }
     }
-    dots_set.len()
+    dots_set
 }
 
 fn part1(input: &str) -> usize {
-    fold_paper(input, true)
+    fold_paper(input, true).len()
+}
+
+fn part2(input: &str) {
+    let mut grid = [[false; WIDTH]; HEIGHT];
+    for (x, y) in fold_paper(input, false).into_iter() {
+        if x >= WIDTH || y >= HEIGHT { panic!("grid too small"); }
+        grid[y][x] = true;
+    }
+    for line in grid.iter().filter_map(|row|{
+        if row.iter().all(|p| *p == false) { None }
+        else {
+            Some(row.iter().map(|p| if *p { '#' } else { ' ' }).collect::<String>())
+        }
+    }) {
+        println!("{}", &line);
+    }
 }
 
 pub fn solve() {
     println!("part1: {}", part1(INPUT));
+    part2(INPUT);
 }
 
 #[cfg(test)]
@@ -53,5 +72,6 @@ mod tests {
     fn test() {
         const EXAMPLE1: &str = include_str!("day13-ex1.txt");
         assert_eq!(part1(EXAMPLE1), 17);
+        part2(EXAMPLE1);
     }
 }
